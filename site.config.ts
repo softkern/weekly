a7c1f1834e91cf4edee40908f72f354d633eaf73
softkern <hi@softkern.com>
2024-12-10 19:44:43 +0800

    feat: change site config

--- site.config.ts
+++ site.config.ts
@@ -1,8 +1,8 @@
 import type { Site } from '@types'
 
 const SITE: Site = {
-  url: 'https://localhost:4321',
-  title: '个人技术周刊',
+  url: 'https://weekly.softkern.com',
+  title: '软核周刊',
   description: '分享有趣和有价值的技术内容',
 }
 
